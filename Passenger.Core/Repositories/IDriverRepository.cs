3e1fa638be759d4c1f29f6bef01dc87cf1e8d70b
Patryk Huzarski <phuzarski@gmail.com>
2017-03-30 17:02:42 +0200

    Async IDriverRepo

--- Passenger.Core/Repositories/IDriverRepository.cs
+++ Passenger.Core/Repositories/IDriverRepository.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Passenger.Core.Domain;
 
 namespace Passenger.Core.Repositories
 {
     public interface IDriverRepository
     {
-        Driver Get(Guid userId); 
-        IEnumerable<Driver> GetAll();
-        void Add(Driver driver);
-        void Update(Driver driver);
+        Task<Driver> GetAsync(Guid userId); 
+        Task<IEnumerable<Driver>> GetAllAsync();
+        Task AddAsync(Driver driver);
+        Task UpdateAsync(Driver driver);
     }
 }
