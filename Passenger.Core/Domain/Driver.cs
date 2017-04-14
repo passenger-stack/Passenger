a8cced3a2aa467c1188a191723cf1f0b74cdc40f
Patryk Huzarski <phuzarski@gmail.com>
2017-04-14 19:23:13 +0200

    Modeling Domain Class

--- Passenger.Core/Domain/Driver.cs
+++ Passenger.Core/Domain/Driver.cs
@@ -19,17 +19,5 @@ namespace Passenger.Core.Domain
         {
             UserId = userid;
         }
-
-        // w jaki sposob mozemy je konstruować
-        // a) porobić odp konstruktory
-        // b) przemysleć jakie dane mogą być przekazywane jako argumenty
-        // c) jaka walidacja:
-        //uzytkownik nie moze byc nullem
-        // uzytkownik wywala ten adres email i haslo ktore jest puste
-        // OPAKOWAC KLASY KONSTRUKTORAMI, jakieś metody do SETTOW, do walidacji bazujacej na podstawowych wyjątkach
-        // pomysleć o pierwszych interakcjach ktore mogą zachodzić pomiedzy jedną klasa a drugą
-        // np klasa driver konstrujemy ja za pomoca usera,
-        // mozna pomyslec czy klasa user moze miec taka wlasciwosc jak driver
-        // nasza klasa reprezentujaca route albo dailyroute powinna miec metody w stylu ustaw sciezke, dodaj pkt odbioru pasazera, usun pkt odbioru pasazerow.
     }
 }
