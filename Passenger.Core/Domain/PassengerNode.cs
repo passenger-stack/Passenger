b1c3a8011e585eba20d85ba3cdbc9f5dbe29f345
phuzarski <phuzarski@gmail.com>
2017-04-26 15:48:49 +0200

    Route create

--- Passenger.Core/Domain/PassengerNode.cs
+++ Passenger.Core/Domain/PassengerNode.cs
@@ -1,9 +1,16 @@
+using System;
+
 namespace Passenger.Core.Domain
 {
     public class PassengerNode
     {
         public Node Node { get; protected set; }
         public Passenger Passenger { get; protected set; }
+
+        internal static PassengerNode Create(Passenger passenger, Node node)
+        {
+            throw new NotImplementedException();
+        }
 
         protected PassengerNode()
         {
--- Passenger.Core/Domain/Route.cs
+++ Passenger.Core/Domain/Route.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Passenger.Core.Domain
 {
@@ -7,5 +8,19 @@ namespace Passenger.Core.Domain
         public Guid Id { get; protected set; }
         public Node StartNode { get; protected set; }
         public Node EndNode { get; protected set; }
+
+        protected Route()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        protected Route(Node startnode, Node endnode)
+        {
+            StartNode = startnode;
+            EndNode = endnode;
+        }
+
+        public static Route Create(Node startnode, Node endnode)
+            => new Route(startnode, endnode);
     }
 }
